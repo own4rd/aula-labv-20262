95d3348024c0768039b27dc2cd9cf0af932c44ff
Wagner <wagnerpalmeira.ti@gmail.com>
2026-09-15 13:56:52 -0300

    Refact: adding secret key

--- accounts/src/main/java/com/example/accounts/service/impl/JwtService.java
+++ accounts/src/main/java/com/example/accounts/service/impl/JwtService.java
@@ -2,26 +2,30 @@ package com.example.accounts.service.impl;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Service
 public class JwtService {
 
-    private static final String SECRET_KEY =
-            "minha-chave-super-secreta-com-pelo-menos-32-bytes";
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
 
     public String generateToken(UserDetails userDetails) {
         return Jwts.builder()
                 .subject(userDetails.getUsername())
                 .issuedAt(new Date())
                 .expiration(
-                        new Date(System.currentTimeMillis() + 1000 * 60 * 60)
+                        new Date(System.currentTimeMillis() + expiration)
                 )
                 .signWith(getSigningKey())
                 .compact();
@@ -56,7 +60,7 @@ public class JwtService {
     }
 
     private SecretKey getSigningKey() {
-        byte[] keyBytes = SECRET_KEY.getBytes(StandardCharsets.UTF_8);
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
 
         return Keys.hmacShaKeyFor(keyBytes);
     }
